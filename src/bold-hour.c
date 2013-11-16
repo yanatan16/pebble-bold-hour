0d3886002e700286ec428b0938d7df42d2ab0676
Jon Eisen <jon@metricfeat.com>
2013-11-15 22:56:04 -0500

    Upgrade bold-hour-white to 2.0

--- src/bold-hour.c
+++ src/bold-hour.c
@@ -8,33 +8,19 @@
    This watch's load/unload code is mostly taken from the big_time watchface which has to
    load/unload images as necessary. The same is true for bold-hour.
 
-   Author: Jon Eisen
+   24 hour clock not supported
+
+   Author: Jon Eisen <jon@joneisen.me>
 
  */
 
-#include "pebble_os.h"
-#include "pebble_app.h"
-#include "pebble_fonts.h"
+#include <pebble.h>
 
-
-#define MY_UUID { 0xA3, 0x33, 0x71, 0xE8, 0x33, 0xCB, 0x42, 0xD2, 0x8E, 0x91, 0xC6, 0x6F, 0x26, 0x72, 0xE5, 0xF2 }
-PBL_APP_INFO(MY_UUID,
-             "Bold Hour", "Jon Eisen",
-             1, 2, /* App version */
-             RESOURCE_ID_IMAGE_MENU_ICON,
-             APP_INFO_WATCH_FACE);
-
-Window window;
-
-TextLayer minuteLayer;
-GRect minuteFrame;
-
-#define UNINITTED -1
-int image_state = UNINITTED;
-
-BmpContainer imageContainer;
-
+// -- build configuration --
 #define LIGHT_WATCHFACE
+
+// -- macros --
+#define UNINITTED -1
 
 #ifdef LIGHT_WATCHFACE
   #define TEXT_COLOR GColorWhite
@@ -44,10 +30,19 @@ BmpContainer imageContainer;
   #define BKGD_COLOR GColorWhite
 #endif
 
-#define NUMBER_OF_IMAGES 12
+// -- Global assets --
+Window *window;
+
+TextLayer *minuteLayer;
+BitmapLayer *hourLayer;
+
+GRect minuteFrame;
+GBitmap *hourImage;
+
+int loaded_hour;
 
 // These are all of our images. Each is the entire screen in size.
-const int IMAGE_RESOURCE_IDS[NUMBER_OF_IMAGES] = {
+const int IMAGE_RESOURCE_IDS[12] = {
   RESOURCE_ID_IMAGE_NUM_1, RESOURCE_ID_IMAGE_NUM_2,
   RESOURCE_ID_IMAGE_NUM_3, RESOURCE_ID_IMAGE_NUM_4, RESOURCE_ID_IMAGE_NUM_5,
   RESOURCE_ID_IMAGE_NUM_6, RESOURCE_ID_IMAGE_NUM_7, RESOURCE_ID_IMAGE_NUM_8,
@@ -57,60 +52,39 @@ const int IMAGE_RESOURCE_IDS[NUMBER_OF_IMAGES] = {
 
 
 void load_digit_image(int digit_value) {
-  /*
-     Loads the digit image from the application's resources and
-     displays it on-screen in the correct location.
-
-     Each slot is the whole screen.
-
-   */
-
   if ((digit_value < 1) || (digit_value > 12)) {
     return;
   }
 
-  if (image_state == UNINITTED) {
-    bmp_init_container(IMAGE_RESOURCE_IDS[digit_value-1], &imageContainer);
-    imageContainer.layer.layer.frame.origin.x = 0;
-    imageContainer.layer.layer.frame.origin.y = 0;
-    layer_add_child(&imageContainer.layer.layer, &minuteLayer.layer);
-    layer_add_child(&window.layer, &imageContainer.layer.layer);
-    image_state = digit_value;
+  if (!hourImage) {
+    hourImage = gbitmap_create_with_resource(IMAGE_RESOURCE_IDS[digit_value-1]);
+    bitmap_layer_set_bitmap(hourLayer, hourImage);
+    loaded_hour = digit_value;
   }
 
 }
 
 void unload_digit_image() {
-  /*
 
-     Removes the digit from the display and unloads the image resource
-     to free up RAM.
-
-   */
-
-  if (image_state != UNINITTED) {
-    layer_remove_child_layers(&imageContainer.layer.layer);
-    layer_remove_from_parent(&imageContainer.layer.layer);
-    bmp_deinit_container(&imageContainer);
-    image_state = UNINITTED;
+  if (hourImage) {
+    gbitmap_destroy(hourImage);
+    hourImage = 0;
+    loaded_hour = 0;
   }
 
 }
 
-void reinit_text_layer(unsigned short horiz) {
+void set_minute_layer_location(unsigned short horiz) {
   if (minuteFrame.origin.x != horiz) {
     minuteFrame.origin.x = horiz;
-    layer_set_frame(&minuteLayer.layer, minuteFrame);
-    layer_mark_dirty(&minuteLayer.layer);
+    layer_set_frame(text_layer_get_layer(minuteLayer), minuteFrame);
+    layer_mark_dirty(text_layer_get_layer(minuteLayer));
   }
 }
 
-void display_time(PblTm * tick_time) {
+void display_time(struct tm * tick_time) {
 
   // 24 hour clock not supported
-  // if (clock_is_24h_style()) {
-  //   return hour;
-  // }
 
   unsigned short hour = tick_time->tm_hour % 12;
 
@@ -118,7 +92,7 @@ void display_time(PblTm * tick_time) {
   hour = hour ? hour : 12;
 
   // Only do memory unload/load if necessary
-  if (image_state != hour) {
+  if (loaded_hour != hour) {
     unload_digit_image();
     load_digit_image(hour);
   }
@@ -126,66 +100,63 @@ void display_time(PblTm * tick_time) {
   // Show minute
   static char text[] = "00";
 
-  string_format_time(text, sizeof(text), "%M", tick_time);
+  strftime(text, sizeof(text), "%M", tick_time);
 
   unsigned short n1s = (text[0]=='1') + (text[1]=='1');
 
   if (hour == 10 || hour == 12) {
-    reinit_text_layer(70 + 3*n1s);
+    set_minute_layer_location(70 + 3*n1s);
   } else {
-    reinit_text_layer(53 + 3*n1s);
+    set_minute_layer_location(53 + 3*n1s);
   }
 
-  text_layer_set_text(&minuteLayer, text);
+  text_layer_set_text(minuteLayer, text);
 }
 
 
-void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *t) {
-  (void)t;
-  (void)ctx;
-
-  display_time(t->tick_time);
+void handle_minute_tick(struct tm * tick_time, TimeUnits units_changed) {
+  display_time(tick_time);
 }
 
-void handle_init(AppContextRef ctx) {
-  (void)ctx;
-
-  window_init(&window, "Bold Hour");
-  window_stack_push(&window, true /* Animated */);
-  window_set_background_color(&window, BKGD_COLOR);
-
-  resource_init_current_app(&APP_RESOURCES);
+void handle_init() {
 
   minuteFrame = GRect(53, 23, 40, 40);
 
-  text_layer_init(&minuteLayer, window.layer.frame);
-  text_layer_set_text_color(&minuteLayer, TEXT_COLOR);
-  text_layer_set_background_color(&minuteLayer, GColorClear);
-  text_layer_set_font(&minuteLayer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_MINUTE_38)));
-  layer_set_frame(&minuteLayer.layer, minuteFrame);
+  // Dynamic allocation of assets
+  window = window_create();
+  minuteLayer = text_layer_create(minuteFrame);
+  hourLayer = bitmap_layer_create(GRect(0, 0, 144, 168));
+
+  // Configure window
+  window_stack_push(window, true /* Animated */);
+  window_set_background_color(window, BKGD_COLOR);
+  window_set_fullscreen(window, true);
+
+  // Setup minute layer
+  text_layer_set_text_color(minuteLayer, TEXT_COLOR);
+  text_layer_set_background_color(minuteLayer, GColorClear);
+  text_layer_set_font(minuteLayer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_MINUTE_38)));
+
+  // Setup layers
+  layer_add_child(bitmap_layer_get_layer(hourLayer), text_layer_get_layer(minuteLayer));
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(hourLayer));
 
   // Avoids a blank screen on watch start.
-  PblTm tick_time;
-  get_time(&tick_time);
-
-  display_time(&tick_time);
+  time_t tick_time = time(NULL);
+  display_time(localtime(&tick_time));
 }
 
-void handle_deinit(AppContextRef ctx) {
-  (void)ctx;
-
+void handle_deinit() {
   unload_digit_image();
+
+  text_layer_destroy(minuteLayer);
+  bitmap_layer_destroy(hourLayer);
+  window_destroy(window);
 }
 
-void pbl_main(void *params) {
-  PebbleAppHandlers handlers = {
-    .init_handler = &handle_init,
-    .deinit_handler = &handle_deinit,
-
-    .tick_info = {
-      .tick_handler = &handle_minute_tick,
-      .tick_units = MINUTE_UNIT
-    }
-  };
-  app_event_loop(params, &handlers);
+int main(void) {
+  handle_init();
+  tick_timer_service_subscribe(MINUTE_UNIT, &handle_minute_tick);
+  app_event_loop();
+  handle_deinit();
 }
